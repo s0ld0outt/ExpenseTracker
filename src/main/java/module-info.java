054bf2c9ee092ad88614b25970e21ffba60da137
s0ld0outt <fl1ckxexexe@yandex.ru>
2024-12-16 17:30:25 +0300

    module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.example.expensetracker {
     requires javafx.fxml;
     requires org.apache.poi.poi;
     requires org.apache.poi.ooxml;
+    requires javafx.graphics;
 
     opens com.example.expensetracker to javafx.fxml;
     exports com.example.expensetracker;
